a154bc4d56036a285ff710ed58406477c8a763fc
Joao Victor <joaovictor.cogomes@gmail.com>
2022-03-17 09:59:17 -0300

    Inicializando repositorio remoto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'clientes-servicos-app-2';
+
+  ngAfterViewInit(): void {
+    (($) => {
+      "use strict"; // Start of use strict
+
+      // Toggle the side navigation
+      $("#sidebarToggle, #sidebarToggleTop").on('click', function (e: any) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+          $('.sidebar .collapse').collapse('hide');
+        };
+      });
+
+      // Close any open menu accordions when window is resized below 768px
+      $(window).resize(function () {
+        if ($(window).width() < 768) {
+          $('.sidebar .collapse').collapse('hide');
+        };
+
+        // Toggle the side navigation when window is resized below 480px
+        if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+          $("body").addClass("sidebar-toggled");
+          $(".sidebar").addClass("toggled");
+          $('.sidebar .collapse').collapse('hide');
+        };
+      });
+
+      /*
+      // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+      $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', (e: { originalEvent: any; preventDefault: () => void; }) => {
+        if ($(window).width() > 768) {
+          var e0 = e.originalEvent,
+            delta = e0.wheelDelta || -e0.detail;
+          this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+          e.preventDefault();
+        }
+      });
+      */
+
+      // Scroll to top button appear
+      $(document).on('scroll', () => {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+          $('.scroll-to-top').fadeIn();
+        } else {
+          $('.scroll-to-top').fadeOut();
+        }
+      });
+
+      // Smooth scrolling using jQuery easing
+      $(document).on('click', 'a.scroll-to-top', (e: { preventDefault: () => void; }) => {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1000, 'easeInOutExpo');
+        e.preventDefault();
+      });
+
+    })($); // End of use strict
+
+  }
 }
